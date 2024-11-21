bfa359487882d116b219203ada77376f4fa6d931
loooonnngg <long.vo2@metropolia.fi>
2024-11-21 09:41:37 +0200

    module 3 completed

--- Module 4/t4/4.js
+++ Module 4/t4/4.js
@@ -14,13 +14,14 @@ async function getShow(evt){
     let link=document.createElement('a')
     let image=document.createElement('img')
     let summary=document.createElement('div')
+    let lineBreak = document.createElement('br')
     name.innerText=show.show.name
     link.innerText=show.show.url
     link.target='_blank'
     show.show.image ? image.src=show.show.image.medium : image.src='https://via.placeholder.com/210x295?text=Not%20Found'
     image.alt=show.show.name
     summary.innerHTML=show.show.summary
-    article.appendChild(name);article.appendChild(link);article.appendChild(image);article.appendChild(summary)
+    article.appendChild(name);article.appendChild(link);article.appendChild(lineBreak);article.appendChild(image);article.appendChild(summary)
     result.appendChild(article)
   }
     } catch (error) {
